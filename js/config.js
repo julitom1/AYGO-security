cfd7dbfb127a45fdc61c441580077eca8d3ac6ad
lab lab <lab@labinfo.is.escuelaing.edu.co>
2024-11-16 09:58:29 -0500

    new_config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,8 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_hUuoqXf10', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '69d1qvj2qs3c7e31bavaect7qt
-', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1
-' // e.g. us-east-2
+        userPoolClientId: '69d1qvj2qs3c7e31bavaect7qt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
